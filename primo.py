baf1a30d1d13a4ed8d26d5bc956d77fc273396d5
ThomasTurbato89 <davidecalcagni.dc@gmail.com>
2024-02-14 17:26:36 +0100

    aggiunta la distanza

--- primo.py
+++ primo.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import math
+
+def calcola_ipotenusa(cateto1, cateto2):
+    ipotenusa = math.sqrt(cateto1**2 + cateto2**2)
+    return ipotenusa
+
+if __name__ == "__main__":
+    cateto1 = float(input("Inserisci la lunghezza del primo cateto: "))
+    cateto2 = float(input("Inserisci la lunghezza del secondo cateto: "))
+    
+    ipotenusa = calcola_ipotenusa(cateto1, cateto2)
+    print("L'ipotenusa è:", ipotenusa)
+
+    
