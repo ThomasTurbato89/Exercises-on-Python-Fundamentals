ba5b168aed35e73914ae3232705ef07de495ef38
ThomasTurbato89 <davidecalcagni.dc@gmail.com>
2024-02-22 15:14:18 +0100

    sium

---
+++ secondo.py
@@ -0,0 +1,23 @@
+intero1 = 1
+intero2 = 1
+stringa1 = "stringa1"
+stringa2 = "stringa2"
+float1 = 1.3
+float2 = 1.3
+bool1 = True
+bool2 = False
+
+print("intero ",intero1+intero2)#fa la somma e viene 2
+print("stringa ",stringa1+stringa2)#stampa le due stringhe attaccate
+print("float ",float1+float2)#fa la somma e viene 2.6
+print("bool",bool1+bool2)#è praticamente un or
+print("intero1 e stringa1 ",intero1+stringa1)#impossibile
+print("stringa1 e intero2",stringa2+intero1)#impossibile
+print("intero1 e float2 ",intero1+float2)
+print("float2 e intero2 ",float2+intero2)
+print("intero1 e bool1 ",intero1+bool1)
+print("float1 e bool1 ", float1+bool1)
+print("stringa1 e bool1 ", stringa1+bool1)#impossibile
+print("bool1 e intero1 ",bool1+intero1)
+print("bool1 e stringa1 ",bool1+stringa1)#impossibile
+print("bool1 e float1 ", bool1+float1)
